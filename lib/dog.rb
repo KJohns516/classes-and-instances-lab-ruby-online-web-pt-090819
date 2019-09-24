11aafe5c791141f501e284f0168964f1b04d2fd3
Kristan Johns <kristandjohns@gmail.com>
2019-09-24 20:28:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+end
+  
+Fido = Dog.new
+Fido => #<Dog:windows file explorer/documents/dogphotos
+
+Snoopy = Dog.new
+Snoopy => #<Dog:windows file explorer/documents/dogphotos
+
+Lassie = Dog.new
+Fido => #<Dog:winodws file explorer/documents/dog dogphotos
+
+Lassie = Snoopy =>#false - these dogs are not the same.
+
+Fido = Dog =>#<Dog: True - Fido is a dog.
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+Class Person
+end
+
+Kristan = Person.new
+Kristan => #Person: 123456789
+
+
+Christopher = Person.new
+Christopher => #Person: 12345678
+
+Chrisopher = Kristan =>#false: because both are just same they're only names.
+
+Christopher = Person =>#true: Christopher is a human.
